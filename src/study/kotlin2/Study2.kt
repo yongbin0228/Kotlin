663be8585d5c6fa8555cd24d82047eeaf54e6a04
Yongbin <bin3525@naver.com>
2019-07-04 15:04:32 +0900

    new

--- src/study/kotlin2/Study2.kt
+++ src/study/kotlin2/Study2.kt
@@ -1,16 +1,41 @@
 package study.kotlin2
 
+import java.util.Arrays.asList
+
 fun main() {
-    val list = listOf("Hello","Kotlin")
+    val list = listOf("Hello", "Kotlin")
     val sb = StringBuffer();
-    for(str in list){
+    for (str in list) {
         sb.append(str)
     }
     println(sb)
 
     val list1 = listOf("Hello", "Kotlin", "Hi")
-    for((index, value) in list1.withIndex()){
+    for ((index, value) in list1.withIndex()) {
         println("$index -> $value")
     }
 
+    label@ for (i in 1..10) {
+        for (j in 1..10) {
+            print("($i, $j) ")
+            if (i == 5 && j == 5)
+                break@label
+        }
+        println()
+    }
+    println()
+    val a = arrayOf("Hello", "Kotlin", "I", "am", "JAVA")
+    some(*a)
+
+    val array1 = arrayOf(10, 20, 30)
+
+    val list2 = asList(1, 2, *array1, 120, 200)
+    list2.forEach{ println(it) }
+}
+
+fun some(vararg a: String) {
+    val i = a.iterator();
+    while (i.hasNext()) {
+        println(i.next())
+    }
 }
